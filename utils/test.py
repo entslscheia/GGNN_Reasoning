b14c0727543a411b785e9ef504c41b979d40e19d
entslscheia <yugu772@yahoo.com>
2018-10-16 21:40:52 +0100

    fix the output of test loss

--- utils/test.py
+++ utils/test.py
@@ -3,9 +3,11 @@ from torch.autograd import Variable
 
 def test(dataloader, dataset, net, criterion, opt):
     test_loss = 0
+    sample_count = 0
     correct = 0
     net.eval()  # set the evaluation mode. It's necessary when using something like dropout
     for i, (annotation, A_dummy, target, data_idx) in enumerate(dataloader, 0):
+        sample_count += len(A_dummy)
         annotation_dim = annotation.shape[2]
         padding = torch.zeros(len(annotation), opt.n_node, opt.state_dim - annotation_dim).double()
         init_input = torch.cat((annotation.double(), padding), 2)   # [batch_size, self.n_node, state_dim]
@@ -21,7 +23,7 @@ def test(dataloader, dataset, net, criterion, opt):
         target = target.double()
         output = net(init_input, annotation, A)
         # print(criterion(output, target).data)
-        test_loss += criterion(output, target).data.item()
+        test_loss += criterion(output, target).data.item()*len(A_dummy)
         # print('output: ', output)
         labels = []
         for i in range(output.shape[0]):
@@ -34,7 +36,7 @@ def test(dataloader, dataset, net, criterion, opt):
             pred = pred.cuda()
         correct += pred.eq(target.data).cpu().sum()
 
-    test_loss /= len(dataloader.dataset)
+    test_loss /= sample_count
     print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
         test_loss, correct, len(dataloader.dataset),
         100. * correct / len(dataloader.dataset)))
